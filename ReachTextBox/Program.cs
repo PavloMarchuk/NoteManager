389fbe0ec5083cef3478154458a9861c8e942ef4
Pavlo Marchuk <Pavlo Marchuk>
2017-03-09 11:01:40 +0200

    Додано текстове поле авторизації

--- ReachTextBox/Program.cs
+++ ReachTextBox/Program.cs
@@ -14,6 +14,7 @@ namespace ReachTextBox
 		[STAThread]
 		static void Main()
 			{
+			// комент для git (test)
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
